b867793f69e4ac77f8e6eb2df9f4d2b362efedec
Harsh223 <harsa.blk@gmail.com>
2021-03-16 00:53:52 +0100

    commit

---
+++ app/src/main/java/com/anshika/expenseguru/activities/MainActivity.java
@@ -0,0 +1,60 @@
+package com.anshika.expenseguru.activities;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.View;
+
+import com.anshika.expenseguru.R;
+import com.anshika.expenseguru.adapters.SectionsPageAdapter;
+import com.anshika.expenseguru.fragments.BalanceFragment;
+import com.anshika.expenseguru.fragments.CustomBottomSheetDialogFragment;
+import com.anshika.expenseguru.fragments.ExpenseFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+
+    public static FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mViewPager=findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout=findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+
+         fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CustomBottomSheetDialogFragment().show(getSupportFragmentManager(), "Dialog");
+
+            }
+        });
+
+    }
+
+
+
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter=new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ExpenseFragment(),"Expenses");
+        adapter.addFragment(new BalanceFragment(),"Balance");
+        viewPager.setAdapter(adapter);
+    }
+
+
+
+}
